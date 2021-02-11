052ca9fd82643ea13013f95fa988754ceddb8dbd
Glitch (hello-webpage) <none>
2021-02-11 13:53:45 +0000

    🌨🍫 Checkpoint ./views/dashboard.hbs:27800253/84 ./package.json:27800253/710 ./server.js:27800253/1066

---
+++ server.js
@@ -0,0 +1,31 @@
+
+// use javascript in strict mode
+'use strict';
+
+// import all required modules
+const express = require('express');
+const logger = require('./utils/logger');
+const exphbs = require('express-handlebars');
+
+// initialise project
+const app = express();
+
+// static files output to public folder
+app.use(express.static('public'));
+
+// use handlebars as view engine
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+}));
+app.set('view engine', '.hbs');
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 4000, function () {
+  logger.info(`glitch-playlist1 started on port ${listener.address().port}`);
+});
