69bd42c32b639ef2bb688b0083926d0cc5ce7bcc
Glitch (hello-webpage) <none>
2021-02-14 14:19:00 +0000

    🎌🐒 Checkpoint ./controller.dashboard.js:27800253/1291

--- controller.dashboard.js
+++ controller.dashboard.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict";
 
 // import all required modules
-const logger = require('../utils/logger');
+const logger = require("../utils/logger");
 
 // create dashboard object
 const dashboard = {
-
   // index method - responsible for creating and rendering the view
   index(request, response) {
-
     // display confirmation message in log
-    logger.info('dashboard rendering');
+    logger.info("dashboard rendering");
 
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
-      title: 'Playlist App Dashboard',
+      title: "Playlist App Dashboard"
     };
 
     // render the dashboard view and pass through the data
-    response.render('dashboard', viewData);
-  },
+    response.render("dashboard", viewData);
+  }
 };
 
 // export the dashboard module
