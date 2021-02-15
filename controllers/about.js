2d87b1d33da8636c7ceca09ea71f2af96756511a
Glitch (hello-webpage) <none>
2021-02-15 20:26:51 +0000

    👌👢 Checkpoint ./routes.js:27800253/985 ./views/partials/menu.hbs:27800253/39 ./controllers/about.js:27800253/1280

--- controllers/about.js
+++ controllers/about.js
@@ -1,26 +1,24 @@
-'use strict';
+"use strict";
 
 // import all required modules
-const logger = require('../utils/logger');
+const logger = require("../utils/logger");
 
-// create dashboard object
+// create start object
 const about = {
-
   // index method - responsible for creating and rendering the view
   index(request, response) {
-
     // display confirmation message in log
-    logger.info('about rendering');
+    logger.info("about rendering");
 
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
-      title: 'about App ',
+      title: "Welcome to the Playlist App!"
     };
 
-    // render the dashboard view and pass through the data
-    response.render('about', viewData);
-  },
+    // render the start view and pass through the data
+    response.render("about", viewData);
+  }
 };
 
-// export the dashboard module
+// export the start module
 module.exports = about;
--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // import express and initialise router
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // import controllers
-const start = require('./controllers/start.js');
-const dashboard = require('./controllers/dashboard.js');
+const start = require("./controllers/start.js");
+const dashboard = require("./controllers/dashboard.js");
 
 // connect routes to controllers
-router.get('/', start.index);
-router.get('/dashboard', dashboard.index);
-router.get('/', about.index);
+router.get("/", start.index);
+router.get("/dashboard", dashboard.index);
+router.get("/", about.index);
 // export router module
 module.exports = router;
