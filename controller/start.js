c0aaf8e3b9c6491797be44c5f7e55630f63ee0ba
Glitch (hello-webpage) <none>
2021-02-15 19:48:37 +0000

    🏘👞 Checkpoint ./controller/start.js:27800253/2618

--- controller/start.js
+++ controller/start.js
@@ -1,25 +1,24 @@
-'use strict';
+"use strict";
 
 // import all required modules
-const logger = require('../utils/logger');
+const logger = require("../utils/logger");
+
 
 // create start object
 const start = {
-
   // index method - responsible for creating and rendering the view
   index(request, response) {
-
     // display confirmation message in log
-    logger.info('start rendering');
+    logger.info("start rendering");
 
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
-      title: 'Welcome to the Playlist App!',
+      title: "Welcome to the Playlist App!"
     };
 
     // render the start view and pass through the data
-    response.render('start', viewData);
-  },
+    response.render("start", viewData);
+  }
 };
 
 // export the start module
