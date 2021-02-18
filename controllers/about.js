32c379fd200f200039dd8cadb15e92bd754a4adb
Glitch (hello-webpage) <none>
2021-02-18 14:27:57 +0000

    💎🌧 Checkpoint ./controllers/about.js:27800253/473

--- controllers/about.js
+++ controllers/about.js
@@ -2,7 +2,7 @@
 
 // import all required modules
 const logger = require('../utils/logger');
-
+const developerStore = require('../models/developer-store.js');
 // create dashboard object
 const about = {
 
@@ -15,9 +15,11 @@ const about = {
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
       title: 'About App Board',
+      developerStore: developerStore.getAllDevelopers(),
     };
 
     // render the dashboard view and pass through the data
+    logger.info('about to render', viewData.developerStore);
     response.render('about', viewData);
   },
 };
