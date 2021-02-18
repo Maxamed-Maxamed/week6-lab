b22dec264988fde788c5477e7fc8ed1d2cba775d
Glitch (hello-webpage) <none>
2021-02-18 15:14:24 +0000

    🏃🎥 Checkpoint ./controllers/about.js:27800253/1590 ./views/about.hbs:27800253/3

--- controllers/about.js
+++ controllers/about.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
 
 // import all required modules
-const logger = require('../utils/logger');
-const developerStore = require('../models/developer-store.js');
+const logger = require("../utils/logger");
+const developerStore = require("../models/developer-store.js");
 // create dashboard object
 const about = {
-
   // index method - responsible for creating and rendering the view
   index(request, response) {
-
     // display confirmation message in log
-    logger.info('about rendering');
+    logger.info("about rendering");
 
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
-      title: 'About App Board',
-      developers: developerStore.getAllDevelopers(),
+      title: "About App Board",
+      developers: developerStore.getAllDevelopers()
     };
 
     // render the dashboard view and pass through the data
-    logger.info('about to render', viewData.developers);
-    response.render('about', viewData);
-  },
+    logger.info("about to render", viewData.developers);
+    response.render("about", viewData);
+  }
 };
 
 // export the dashboard module
