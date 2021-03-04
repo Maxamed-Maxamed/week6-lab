e7c6b5cbb188da8f0976f4d158405731b356093b
Glitch (lab5-week5) <none>
2021-03-04 13:55:39 +0000

    🌽🏍 Checkpoint ./controllers/playlist.js:27800253/2128 ./models/playlist-store.js:27800253/894 ./package.json:27800253/26 ./views/playlist.hbs:27800253/293

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,17 +1,18 @@
-'use strict';
+"use strict";
 
-const logger = require('../utils/logger');
-const playlistCollection = require('../models/playlist-store.js');
+const logger = require("../utils/logger");
+const playlistStore = require("../models/playlist-store");
 
 const playlist = {
   index(request, response) {
     const playlistId = request.params.id;
-    logger.debug('Playlist id = ' + playlistId);
+    logger.debug("Playlist id = " + playlistId);
     const viewData = {
-      title: 'Playlist',
+      title: "Playlist",
+      playlist: playlistStore.getPlaylist(playlistId)
     };
-    response.render('playlist', viewData);
-  },
+    response.render("playlist", viewData);
+  }
 };
 
 module.exports = playlist;
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,13 +1,16 @@
-'use strict';
+"use strict";
+const _ = require("lodash");
 
 const playlistStore = {
-
   // import the playlist collection object
-  playlistCollection: require('./playlist-store.json').playlistCollection,
+  playlistCollection: require("./playlist-store.json").playlistCollection,
 
   // function to get all of the playlists
   getAllPlaylists() {
     return this.playlistCollection;
+  },
+  getPlaylist(id) {
+    return _.find(this.playlistCollection, { id: id });
   },
 
 };
