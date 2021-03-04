ff8911af09dff670c722b1054db325bdaa11adb8
Glitch (lab5-week5) <none>
2021-03-04 14:06:49 +0000

    🎌👢 Checkpoint ./views/playlist.hbs:27800253/2694 ./models/playlist-store.js:27800253/802 ./controllers/playlist.js:27800253/889 ./views/partials/listsongs.hbs:27800253/1038

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,19 +1,19 @@
-"use strict";
-const _ = require("lodash");
+'use strict';
+
+const _ = require('lodash');
 
 const playlistStore = {
-  // import the playlist collection object
-  playlistCollection: require("./playlist-store.json").playlistCollection,
 
-  // function to get all of the playlists
+  playlistCollection: require('./playlist-store.json').playlistCollection,
+
   getAllPlaylists() {
     return this.playlistCollection;
   },
-  getPlaylist(id) {
+
+    getPlaylist(id) {
     return _.find(this.playlistCollection, { id: id });
   },
 
 };
 
-// export the playlistStore object so it can be used elsewhere
 module.exports = playlistStore;
